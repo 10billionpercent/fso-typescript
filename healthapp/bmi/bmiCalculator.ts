dd3f8af9f61e04da91e2b069b88bacb5430ddf63
Shreya <shreya501711@gmail.com>
2026-04-14 14:07:54 +0530

    update bmi calculator to accept command line arguments and install tsx for easier path resolution

--- healthapp/bmi/bmiCalculator.ts
+++ healthapp/bmi/bmiCalculator.ts
@@ -1,3 +1,5 @@
+import isNotNumber from '../utils';
+
 const calculateBmi = (height: number, weight: number) : string => {
     const heightInMetres = height /100;
     const bmi = weight / (heightInMetres ** 2);
@@ -12,4 +14,27 @@ const calculateBmi = (height: number, weight: number) : string => {
     }
 }
 
-console.log(calculateBmi(180, 74));
+const height = Number(process.argv[2]);
+const weight = Number(process.argv[3]);
+
+if (process.argv.length < 4) {
+    console.log("too few arguments");
+    process.exit(1);
+}
+
+if (process.argv.length > 4) {
+    console.log("too many arguments");
+    process.exit(1);
+}
+
+if (isNotNumber(height) || isNotNumber(weight)) {
+    console.log("please provide proper numbers");
+    process.exit(1);
+}
+
+if (height === 0 || weight === 0) {
+    console.log("please provide non zero values for height and weight");
+    process.exit(1);
+}
+
+console.log(calculateBmi(height, weight));
