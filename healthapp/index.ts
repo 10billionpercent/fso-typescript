769e3ccbdfef742684cbdb10eb524feee7e0bf27
Shreya <shreya501711@gmail.com>
2026-04-18 10:45:02 +0530

    express app exercises endpoint

--- healthapp/index.ts
+++ healthapp/index.ts
@@ -1,8 +1,15 @@
 import express from 'express';
 import isNotNumber from './utils.ts';
 import calculateBmi from './bmi/bmiCalculator.ts';
+import calculateExercises from './exercise/exerciseCalculator.ts';
 
 const app = express();
+app.use(express.json());
+
+interface exerciseData {
+    daily_exercises : number[];
+    target: number;
+};
 
 app.get('/hello', (_req, res) => {
     res.send(`
@@ -30,6 +37,27 @@ app.get('/bmi', (req, res) => {
         bmi: calculatedBmi
     });
 
+});
+
+app.post('/exercises', (req, res) => {
+    const body = req.body as exerciseData;
+
+    if (!body || body.daily_exercises === undefined || body.target === undefined) {
+        return res.status(400).json({
+            error: "missing parameters"
+        });
+    }
+
+    const { daily_exercises, target } = body;
+
+    if (!Array.isArray(daily_exercises) || daily_exercises.some(e => isNotNumber(e)) || isNotNumber(target)) {
+        return res.status(400).json({
+            error: "malformatted parameters"
+        });
+    }
+
+    const calculatedExercises = calculateExercises(Number(target), daily_exercises);
+    return res.status(200).json(calculatedExercises);
 });
 
 const PORT = 4000;
