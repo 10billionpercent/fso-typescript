31a9c5e07280da2b6cfef249c485f0155658cad2
Shreya <shreya501711@gmail.com>
2026-04-18 15:54:48 +0530

    add diagnosis and patient data, create diagnoses endpoint to view all diagnoses from data/diagnoses.ts

--- patientor/backend/src/index.ts
+++ patientor/backend/src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import cors from 'cors';
+import diagnosisRouter from './routes/diagnoses.ts';
 
 const app = express();
 app.use(express.json());
@@ -8,6 +9,8 @@ app.use(cors());
 app.get('/api/ping', (_req, res) => {
     res.send('meow');
 });
+
+app.use('/api/diagnoses', diagnosisRouter);
 
 const PORT = 3000;
 
