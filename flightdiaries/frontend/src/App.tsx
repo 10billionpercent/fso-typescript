8fbf68a36ca141f663bf98704e6f2c8ad75b55bc
Shreya <shreya501711@gmail.com>
2026-04-26 15:56:47 +0530

    frontend shows all diaries (without comments) and added cors to backend

---
+++ flightdiaries/frontend/src/App.tsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react'
+import type { DiaryEntry } from './types'
+import diaryService from './services/diaries'
+import { Plane, Calendar, Sun, RectangleGoggles } from 'lucide-react';
+import './App.css'
+
+function App() {
+  const [diaries, setDiaries] = useState<DiaryEntry[]>([])
+
+  useEffect(() => {
+    const getInitialData = async () => {
+    const res = await diaryService.getAll()
+    setDiaries(res)
+    }
+    getInitialData()
+  }, [])
+
+  return (
+    <div  id='main'>
+      <h1 className='row'> <Plane size={40}/> Flight Diaries </h1>
+      {diaries.map(d => (
+        <div>
+          <h3 className='row'><Calendar /> {d.date}</h3>
+          <p className='row'><Sun /><strong>Weather</strong> {d.weather}</p>
+          <p className='row'><RectangleGoggles /> <strong>Visibility</strong> {d.visibility}</p>
+          <p className='row'>{d.comment}</p>
+        </div>
+      ))}
+    </div>
+  )
+}
+
+export default App
---
+++ flightdiaries/frontend/src/services/diaries.ts
@@ -0,0 +1,11 @@
+import axios from "axios"
+import type { DiaryEntry } from "../types"
+
+const baseUrl = 'http://localhost:3000/api/diaries';
+
+const getAll = async () => {
+    const res = await axios.get<DiaryEntry[]>(baseUrl);
+    return res.data
+}
+
+export default { getAll }
