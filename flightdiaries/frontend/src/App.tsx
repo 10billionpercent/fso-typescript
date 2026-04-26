cd7791388c8c0b4d5e54592d8031a8f112d70e51
Shreya <shreya501711@gmail.com>
2026-04-26 20:08:17 +0530

    adding diaries from frontend

--- flightdiaries/frontend/src/App.tsx
+++ flightdiaries/frontend/src/App.tsx
@@ -6,6 +6,10 @@ import './App.css'
 
 function App() {
   const [diaries, setDiaries] = useState<DiaryEntry[]>([])
+  const [date, setDate] = useState('')
+  const [weather, setWeather] = useState('')
+  const [visibility, setVisibility] = useState('')
+  const [comment, setComment] = useState('')
 
   useEffect(() => {
     const getInitialData = async () => {
@@ -15,9 +19,26 @@ function App() {
     getInitialData()
   }, [])
 
+  const postDiary = async (e: React.SyntheticEvent) => {
+    e.preventDefault()
+    const diaryToPost = {
+      date,
+      weather,
+      visibility,
+      comment
+    }
+    const postedDiary = await diaryService.create(diaryToPost)
+    setDiaries(diaries.concat(postedDiary))
+    setDate('')
+    setWeather('')
+    setVisibility('')
+    setComment('')
+  }
+
   return (
     <div  id='main'>
       <h1 className='row'> <Plane size={40}/> Flight Diaries </h1>
+      <h2> All Diaries </h2>
       {diaries.map(d => (
         <div>
           <h3 className='row'><Calendar /> {d.date}</h3>
@@ -26,6 +47,31 @@ function App() {
           <p className='row'> <SquarePen /> {d.comment}</p>
         </div>
       ))}
+
+      <form onSubmit={postDiary}>
+      <h2> Add a New Diary </h2>
+        <label>
+          Date &nbsp;
+        <input value={date} 
+        onChange={(e) => setDate(e.target.value)} />
+        </label>
+        <label>
+          Weather &nbsp;
+          <input value={weather} 
+        onChange={(e) => setWeather(e.target.value)} />
+        </label>
+        <label>
+          Visibility &nbsp;
+           <input value={visibility} 
+        onChange={(e) => setVisibility(e.target.value)} />
+        </label>
+        <label>
+          Comment &nbsp;
+          <input value={comment} 
+        onChange={(e) => setComment(e.target.value)} />
+        </label>
+        <button type='submit'> Add </button>
+      </form>
     </div>
   )
 }
--- flightdiaries/frontend/src/services/diaries.ts
+++ flightdiaries/frontend/src/services/diaries.ts
@@ -1,11 +1,15 @@
 import axios from "axios"
-import type { DiaryEntry } from "../types"
+import type { DiaryEntry, NewDiaryEntry } from "../types"
 
 const baseUrl = 'http://localhost:3000/api/diaries';
 
 const getAll = async () => {
-    const res = await axios.get<DiaryEntry[]>(baseUrl);
+    const res = await axios.get<DiaryEntry[]>(baseUrl)
     return res.data
 }
 
-export default { getAll }
+const create = async (newDiary: NewDiaryEntry) => {
+    const res = await axios.post<DiaryEntry>(baseUrl, newDiary)
+    return res.data
+}
+export default { getAll, create }
